0cbfdeb7f99f746e2ae8778aae3be187bc860f75
Lucas L. Lima <lucaslima.webdev@gmail.com>
2022-12-07 00:42:58 -0300

    up

--- assets/js/main.mjs
+++ assets/js/main.mjs
@@ -4,6 +4,50 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore()
 console.log(db)
 
+
+let colecao = 'home'
+let documento = 'home'
+
+const descricao = document.getElementById('story')
+console.log(descricao)
+let buscarBtn = document.getElementById('getDesc')
+console.log('buscar é ' + buscarBtn)
+
+buscarBtn.addEventListener('click', (e) =>{
+    console.log('clicou em ' + e.id)
+    const id = e.target.dataset.id
+    console.log(id)
+    /* if (id){
+        
+    } */
+})
+
+
+
+function getData(collectionName, documentName){
+    var homeCollection = db.collection(collectionName).doc(documentName)
+
+    homeCollection.get().then((doc)=>{
+        if(doc.exists){
+            console.log('Document Data: ', doc.data())
+            descricao.innerText = doc.data().descricao
+        } 
+        else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
+//getData(colecao, documento)
+
+
+
+
+
+
+
 /* db.collection("home").get()
 .then((snapshot) => {
     
@@ -15,24 +59,3 @@ console.log(db)
 }).catch(e =>{
     console.log(e)
 }) */
-let colecao = ''
-let documento = ''
-const descricao = document.getElementById('story')
-
-
-
-var homeCollection = db.collection('home').doc('home')
-
-homeCollection.get().then((doc)=>{
-    if(doc.exists){
-        console.log('Document Data: ', doc.data())
-        descricao.innerText = doc.data().descricao
-    } 
-    else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch((error) => {
-    console.log("Error getting document:", error);
-});
-
